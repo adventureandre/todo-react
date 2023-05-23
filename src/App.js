eba07a26273c4a216c351a453cc2630a556cfdcb
andre <adventureandre@hotmail.com>
2023-05-22 23:03:30 -0300

    criando a lista

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, {useState} from "react";
-
 import './App.css';
+import {MdDelete} from "react-icons/md";
 
 const App = () => {
     const ESCAPE_KEY = 27;
     const ENTER_KEY = 13;
 
+    const initialTodos = [
+        {id: 1, title: "estuda React", checked: false},
+        {id: 2, title: "estudar Ingles", checked: false},
+        {id: 3, title: "vida nova", checked: false}
+    ]
+
+
+    const [todos, ] = useState(initialTodos);
     const [value, setValue] = useState("");
 
     const erase = () => {
@@ -32,7 +40,7 @@ const App = () => {
     return (
         <section id="app" className='container'>
             <header>
-                <h1 className='title'>todo</h1>
+                <h1 className='title'>Todo</h1>
             </header>
             <section className='main'>
                 <input className='new-todo'
@@ -41,6 +49,17 @@ const App = () => {
                        onChange={onChange}
                        onKeyDown={onKeyDown}
                 />
+
+                <ul className='todo-list'>
+                    {todos.map((todo) => (
+                        <li key={todo.id.toString()} >
+                        <span className='todo'>{todo.title}</span>
+                            <button className='remove' type='button'>
+                                <MdDelete size={28}/>
+                            </button>
+                        </li>
+                    ))}
+                </ul>
             </section>
         </section>
     );
